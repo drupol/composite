0d117ea4298ddc7b2a149ef0217e048b90dd31ce
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-12-01 17:47:04 +0100

    Update Table C - Add ManyToOne relation.

--- src/Entity/TableC.php
+++ src/Entity/TableC.php
@@ -57,4 +57,15 @@ class TableC
      * @ORM\Column(type="integer", name="BFK2")
      */
     public $bfk2;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=TableB::class, inversedBy="tableC")
+     * @ORM\JoinColumns(
+     *     @ORM\JoinColumn(name="BFK1", referencedColumnName="BPK1"),
+     *     @ORM\JoinColumn(name="BFK2", referencedColumnName="BPK2"),
+     *     @ORM\JoinColumn(name="AFK1", referencedColumnName="AFK1"),
+     *     @ORM\JoinColumn(name="AFK2", referencedColumnName="AFK2"),
+     * )
+     */
+    public $tableB;
 }
